06a7964efdbe35bbe773047c4d76a665aa2964b8
Alyona Bersenieva <dirtydonut@icloud.com>
2023-02-13 18:27:42 +0100

    changed feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,45 +1,51 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector(".feedback-form");
-const email = document.querySelector("input[name=email]");
-const message = document.querySelector("textarea[name=message]");
+// const email = document.querySelector("input[name=email]");
+// const message = document.querySelector("textarea[name=message]");
 
 const LOCALSTORAGE_KEY = "feedback-form-state";
-const savedValues = localStorage.getItem(LOCALSTORAGE_KEY);
-const savedDataObject = JSON.parse(savedValues);
-
-let formData = {};
+// let formData = {};
 
 form.addEventListener("submit", onFormSubmit);
-form.addEventListener("input", throttle(storageFormData, 500));
+form.addEventListener("input", throttle(onFormInput, 500));
 
 reloadPage();
 
-function storageFormData(e) {
-    formData[e.target.name] = e.target.value;
-  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
-};
-
 function onFormSubmit(e) {
-    e.preventDefault();
-   if (email.value === "" || message.value === "") {
-           return alert(`Всі поля повинні бути заповнені!`);
-  }
+  const { elements: { email, message } } = form;
+  e.preventDefault();
   
-    const savedDatas = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-  console.log(savedDatas);
-    
-    e.currentTarget.reset();
-  localStorage.removeItem(LOCALSTORAGE_KEY);
-    formData = {};
+  if (email.value === "" || message.value === "") {
+      return alert(`Всі поля повинні бути заповнені!`);
+  }
+
+  const result = localStorage.getItem(LOCALSTORAGE_KEY);
+  console.log(result);
+
+  e.target.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY)
 };
 
-function reloadPage() {
-     if (savedValues) {
-       email.value = savedDataObject.email || '';
-       message.value = savedDataObject.message || '';
+function onFormInput(e) {
+  // formData[e.target.name] = e.target.value
+  const { elements: { email, message } } = form;
+  const dataValue = {
+    email: email.value,
+    message: message.value
   }
-  
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(dataValue))
+  // console.log(dataValue)
+};
+
+function reloadPage() { 
+  const { elements: { email, message } } = form;
+  const savedDatas = localStorage.getItem(LOCALSTORAGE_KEY);
+  const storedDatas = JSON.parse(savedDatas)
+  if (savedDatas) {
+    email.value = storedDatas.email;
+    message.value = storedDatas.message;
+  }
 };
 
 
